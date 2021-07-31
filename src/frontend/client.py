9d9486d1469677882a298f9e8eca498b08bbcaec
Angus Pearce <49204021+anguspearce@users.noreply.github.com>
2021-07-31 09:17:08 +0200

    Test

--- src/frontend/client.py
+++ src/frontend/client.py
@@ -1,5 +1,17 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import websockets
 import asyncio
 
+async def hello():
+    uri = "ws://localhost:8765"
+    async with websockets.connect(uri) as websocket:
+        name = input("What's your name? ")
+
+        await websocket.send(name)
+        print(f"> {name}")
+
+        greeting = await websocket.recv()
+        print(f"< {greeting}")
+
+asyncio.get_event_loop().run_until_complete(hello())
