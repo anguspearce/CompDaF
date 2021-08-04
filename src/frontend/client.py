653b1f041257dac4625f0dcaf778e3c5cd8e6e40
Georgeo <georgeosabu@gmail.com>
2021-08-04 08:39:57 +0200

    Main client working

--- src/frontend/client.py
+++ src/frontend/client.py
@@ -11,9 +11,11 @@ from protobufs.python import defs_pb2
 from protobufs.python import enums_pb2
 from protobufs.python import register_viewer_pb2
 
+#util
+from util.message_header import *
+from util.message_provider import *
 
 class Client:
-    global websocket
     
     def __init__(self):
         self.url = "ws://localhost:9001"
@@ -22,47 +24,67 @@ class Client:
     def connectToServer(self):
         async def connect(self):
             try:
-                websocket = await websockets.connect(self.url)
+                self.websocket = await websockets.connect(self.url, ping_interval = None)
             except:
                 print("Failed to connect.")
                 exit(0)
             finally:
-                message, type = self.registerViewer()
+                message, type = construct_register_viewer()
                 
                 #Sending Session Id from register viewer
-                await websocket.send(str(message.session_id))
-                regAck = await websocket.recv()
-                if(regAck == "True"):
+                await self.websocket.send(add_message_header(message,type))
+                regAck = await self.websocket.recv()
+                messageType, messageId, messagePayload = strip_message_header(regAck)
+                if(messagePayload.success == True):
                     print("Register viewer acknowledged by server")
                 else:
                     print("Server failed to acknowledge session ID")
                     exit(0)
-            
-
-        
+                
         asyncio.get_event_loop().run_until_complete(connect(self))
 
+    def listen(self):
+        asyncio.get_event_loop().run_until_complete(self.producer_handler(self.websocket,"yeeet"))
 
-        
+    def consumer(self, message):
+        print(message)
 
-    # async def consumer_handler(websocket, path):
-    #     async for message in websocket:
-    #         await consumer(message)
+    async def producer(self):
+        menu = "1. File Info \n2. File Load\n"
 
-    # async def producer_handler(websocket, path):
-    #     while True:
-    #         message = await producer()
-    #         await websocket.send(message)
+        option = input(menu)
 
-    # async def handler(websocket, path):
-    #     consumer_task = asyncio.ensure_future(consumer_handler(websocket, path))
-    #     producer_task = asyncio.ensure_future(producer_handler(websocket, path))
+        if(option == "1"):
+            return "1"
+        else:
+            return "2"
+            
+    async def consumer_handler(self, websocket, path):
+        print("lol")
+        while True:
+            message = await websocket.recv()
+            self.consumer(message)
+        # async for message self.in websocket:
+        #     await self.consumer(message)
+
+    async def producer_handler(self, websocket, path):
+        while True:
+            message = await self.producer()
+            await websocket.send(message)
+            message = await websocket.recv()
+            print(message)
+
+    # async def handler(self, path):
+    #     consumer_task = asyncio.ensure_future(self.consumer_handler(self.websocket, path))
+    #     producer_task = asyncio.ensure_future(self.producer_handler(self.websocket, path))
     #     done, pending = await asyncio.wait(
     #         [consumer_task, producer_task],
     #         return_when = asyncio.FIRST_COMPLETED,
     #     )
     #     for task in pending:
     #         task.cancel()
+
+
 
     def registerViewer(self):
         message_type = enums_pb2.EventType.REGISTER_VIEWER
--- src/main.py
+++ src/main.py
@@ -7,15 +7,7 @@ def main():
     menu = "1. File Info \n2. File Load"
     client = Client()
     client.connectToServer()
-
-    while True:
-
-        option = input(menu)
-
-        if(option == 1):
-
-            directory = input("Enter directory name")
-            file = input("Enter file name")
+    client.listen()
             
 
 if __name__ == "__main__":
