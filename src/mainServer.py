b66e147415e32fa4f368a9841ba2ba5c716f4ed9
Georgeo <georgeosabu@gmail.com>
2021-08-04 08:40:09 +0200

    Daliuge server working

--- src/mainServer.py
+++ src/mainServer.py
@@ -6,6 +6,4 @@ if __name__ == "__main__":
 
     server = DaliugeServer()
     server.host()
-    # start_server = websockets.serve(server.host, "localhost", 9001)
-    # asyncio.get_event_loop().run_until_complete(start_server)
-    # asyncio.get_event_loop().run_forever()
+    
