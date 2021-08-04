aa209a43a1f4c6b6562bea65c63643da7d84a64d
Georgeo <georgeosabu@gmail.com>
2021-08-04 08:39:10 +0200

    message header and provider
    Co-authored-by: Angus Pearce <anguspearce@users.noreply.github.com>

---
+++ src/util/message_header.py
@@ -0,0 +1,75 @@
+# D FOUCHE
+# UCT CS HONS
+# fchdyl001@myuct.ac.za
+
+import struct
+
+import uuid
+import numpy as np
+
+from protobufs.python import enums_pb2
+from protobufs.python import register_viewer_pb2
+from protobufs.python import open_file_pb2
+from protobufs.python import region_requirements_pb2
+from protobufs.python import region_histogram_pb2
+from protobufs.python import region_stats_pb2
+
+# The ICD protocol version number
+ICD_VERSION = 17
+
+# struct EventHeader {
+#     uint16_t type;
+#     uint16_t icd_version;
+#     uint32_t request_id;
+# };
+EVENT_HEADER = struct.Struct('HHI')
+
+MESSAGE_TYPE_TO_PROTOBUF_OBJ = {
+    enums_pb2.EventType.REGISTER_VIEWER:
+        register_viewer_pb2.RegisterViewer,
+    enums_pb2.EventType.REGISTER_VIEWER_ACK:
+        register_viewer_pb2.RegisterViewerAck,
+    enums_pb2.EventType.OPEN_FILE:
+        open_file_pb2.OpenFile,
+    enums_pb2.EventType.OPEN_FILE_ACK:
+        open_file_pb2.OpenFileAck,
+    enums_pb2.EventType.SET_HISTOGRAM_REQUIREMENTS:
+        region_requirements_pb2.SetHistogramRequirements,
+    enums_pb2.EventType.REGION_HISTOGRAM_DATA:
+        region_histogram_pb2.RegionHistogramData,
+    enums_pb2.EventType.SET_STATS_REQUIREMENTS:
+        region_requirements_pb2.SetStatsRequirements,
+    enums_pb2.EventType.REGION_STATS_DATA:
+        region_stats_pb2.RegionStatsData
+}
+
+
+def add_message_header(msg, msg_type):
+    """Prefix a given protobuf message with its event header.
+
+    :param msg: the protobuf message to be sent
+    :param msg_type: the message type, defined in enums_pb2.EventType
+    :return: the message prefixed with its header
+
+    """
+    header = EVENT_HEADER.pack(
+        msg_type,
+        ICD_VERSION,
+        uuid.uuid4().int % np.iinfo(np.uint32()).max)
+    payload = msg.SerializeToString()
+
+    
+    return header + payload
+
+
+def strip_message_header(data):
+    """Remove event header from given message and parse protobuf object.
+
+    :param data: the raw message to parse
+    :return: a tuple (message type, message id, message)
+
+    """
+    message_type, icd_version, message_id = EVENT_HEADER.unpack(data[:8])
+    message = MESSAGE_TYPE_TO_PROTOBUF_OBJ.get(message_type)()
+    message.ParseFromString(data[8:])
+    return (message_type, message_id, message)
---
+++ src/util/message_provider.py
@@ -0,0 +1,152 @@
+# D FOUCHE
+# UCT CS HONS
+# fchdyl001@myuct.ac.za
+
+import uuid
+import numpy as np
+
+from protobufs.python import defs_pb2
+from protobufs.python import enums_pb2
+from protobufs.python import register_viewer_pb2
+from protobufs.python import open_file_pb2
+from protobufs.python import region_requirements_pb2
+from protobufs.python import region_histogram_pb2
+from protobufs.python import region_stats_pb2
+
+
+STATS_TYPES = [enums_pb2.StatsType.Sum,
+               enums_pb2.StatsType.Mean,
+               enums_pb2.StatsType.Sigma,
+               enums_pb2.StatsType.Min,
+               enums_pb2.StatsType.Max]
+
+
+def construct_register_viewer():
+    """Construct a REGISTER_VIEWER message.
+
+    :return: a tuple (message, message type)
+
+    """
+    message_type = enums_pb2.EventType.REGISTER_VIEWER
+    message = register_viewer_pb2.RegisterViewer()
+    message.session_id = np.uint32(
+        uuid.uuid4().int % np.iinfo(np.uint32()).max)
+    return (message, message_type)
+
+
+def construct_register_viewer_ack(client_session_id):
+    """Construct a REGISTER_VIEWER_ACK message.
+
+    :param client_session_id: the id to use in the ack message
+    :return: a tuple (message, message type)
+
+    """
+    message_type = enums_pb2.EventType.REGISTER_VIEWER_ACK
+    message = register_viewer_pb2.RegisterViewerAck()
+    message.session_id = client_session_id
+    message.success = True
+    return (message, message_type)
+
+
+def construct_open_file(file, directory):
+    """Construct an OPEN_FILE message.
+
+    :param file: the name of the file to open
+    :param directory: the path to the file to open
+    :return: a tuple (message, message type)
+
+    """
+    message_type = enums_pb2.EventType.OPEN_FILE
+    message = open_file_pb2.OpenFile()
+    message.file = file
+    message.directory = directory
+    message.file_id = 1
+    return (message, message_type)
+
+
+def construct_open_file_ack():
+    """Construct an OPEN_FILE_ACK message.
+
+    :return: a tuple (message, message type)
+
+    """
+    message_type = enums_pb2.EventType.OPEN_FILE_ACK
+    message = open_file_pb2.OpenFileAck()
+    message.success = True
+    return (message, message_type)
+
+
+def construct_set_histogram_requirements(num_bins):
+    """Construct a SET_HISTOGRAM_REQUIREMENTS message.
+
+    :param num_bins:  the number of bins for the histogram
+    :return: a tuple (message, message type)
+
+    """
+    message_type = enums_pb2.EventType.SET_HISTOGRAM_REQUIREMENTS
+    message = region_requirements_pb2.SetHistogramRequirements()
+    message.file_id = 1
+    message.region_id = -1
+    histogram_config = region_requirements_pb2.SetHistogramRequirements.HistogramConfig()
+    histogram_config.channel = -1
+    histogram_config.num_bins = num_bins
+    message.histograms.append(histogram_config)
+    return (message, message_type)
+
+
+def construct_region_histogram_data(num_bins, bins, mean, std_dev):
+    """Construct a REGION_HISTOGRAM_DATA message.
+
+    :param num_bins: the number of bins for the histogram
+    :param bins: the bin values for the histogram
+    :param mean: the mean of the histogram
+    :param std_dev: the standard deviation of the histogram
+    :return: a tuple (message, message type)
+
+    """
+    message_type = enums_pb2.EventType.REGION_HISTOGRAM_DATA
+    message = region_histogram_pb2.RegionHistogramData()
+    message.file_id = 1
+    message.region_id = -1
+    message.progress = 1
+    histogram = defs_pb2.Histogram()
+    histogram.num_bins = num_bins if num_bins is not None else -1
+    histogram.bins.extend(bins)
+    histogram.mean = mean
+    histogram.std_dev = std_dev
+    message.histograms.append(histogram)
+    return (message, message_type)
+
+
+def construct_set_stats_requirements():
+    """Construct a SET_STATS_REQUIREMETNS message.
+
+    :return: a tuple (message, message type)
+
+    """
+    message_type = enums_pb2.EventType.SET_STATS_REQUIREMENTS
+    message = region_requirements_pb2.SetStatsRequirements()
+    message.file_id = 1
+    message.region_id = -1
+    for stat in STATS_TYPES:
+        message.stats.append(stat)
+    return (message, message_type)
+
+
+def construct_region_stats_data(stats):
+    """Construct a REGION_STATS_DATA message.
+
+    :param stats: the statistics tuple
+    :return: a tuple (message, message type)
+
+    """
+    message_type = enums_pb2.EventType.REGION_STATS_DATA
+    message = region_stats_pb2.RegionStatsData()
+    message.file_id = 1
+    message.region_id = -1
+    for i in range(len(stats)):
+        stat_value = defs_pb2.StatisticsValue()
+        stat_value.stats_type = STATS_TYPES[i]
+        stat_value.value = stats[i]
+        message.statistics.append(stat_value)
+    return (message, message_type)
