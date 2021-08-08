c98d380a8d0f230e30b7646dd30cb8bca4351f69
Angus Pearce <anguspearce0@gmail.com>
2021-08-08 17:54:08 +0200

    Raftlib include throwing errors

--- src/backend/RaftlibServer.cpp
+++ src/backend/RaftlibServer.cpp
@@ -7,6 +7,8 @@
 
 #include "EventHeader.h"
 
+#include "Raftlib.cpp"
+
 class RaftlibServer
 {
 public:
@@ -19,49 +21,18 @@ public:
     void run()
     {
         uWS::App().ws<PerSocketData>("/*", {/* Settings */
-                                            .compression = uWS::DEDICATED_COMPRESSOR_256KB,
-                                            .maxPayloadLength = 16 * 1024 * 1024,
-                                            .open = [](auto * /*ws*/)
-                                            {
-                /* Open event here, you may access ws->getUserData() which points to a PerSocketData struct */ },
-                                            .message = [](auto *ws, std::string_view message, uWS::OpCode opCode)
-                                            {
-                                                // if (opCode == uWS::OpCode::BINARY) {
-                                                //     std::cout<<"opcode";
-                                                // }
-                                                if (message.length() >= sizeof(carta::EventHeader))
-                                                {
-                                                    carta::EventHeader head = *reinterpret_cast<const carta::EventHeader *>(message.data());
-                                                    const char *event_buf = message.data() + sizeof(carta::EventHeader);
-                                                    int event_length = message.length() - sizeof(carta::EventHeader);
-                                                    CARTA::EventType event_type = static_cast<CARTA::EventType>(head.type);
-                                                    std::cout << event_type << "!\n";
-
-                                                    switch (head.type)
-                                                    {
-                                                    case CARTA::EventType::REGISTER_VIEWER:
-                                                    {
-                                                        CARTA::RegisterViewer message;
-
-                                                        // if (message.ParseFromArray(event_buf, event_length))
-                                                        // {
-                                                        //     std::cout<<"GOOD";
-                                                        //     // session->OnRegisterViewer(message, head.icd_version, head.request_id);
-                                                        // }
-                                                        // else
-                                                        // {
-                                                        //     std::cout<<"BAD";
-
-                                                        // }
-                                                        // break;
-                                                    }
-                                                    }
-                                                }
-                                                ws->send(message, opCode, true);
-                                            },                                            
-                                            .close = [](auto * /*ws*/, int /*code*/, std::string_view /*message*/)
-                                            {
-                /* You may access ws->getUserData() here */ }})
+            .compression = uWS::DEDICATED_COMPRESSOR_256KB,
+            .maxPayloadLength = 16 * 1024 * 1024,
+            .open = [](auto * /*ws*/)
+            {
+            /* Open event here, you may access ws->getUserData() which points to a PerSocketData struct */ },
+            .message = [&](auto *ws, std::string_view message, uWS::OpCode opCode)
+            {
+                OnMessage(ws, message, opCode);
+            },                                            
+            .close = [](auto * /*ws*/, int /*code*/, std::string_view /*message*/)
+            {
+            /* You may access ws->getUserData() here */ }})
             .listen(9001, [](auto *listen_socket)
                     {
                         if (listen_socket)
@@ -70,6 +41,38 @@ public:
                         }
                     })
             .run();
+    }
+
+    void OnMessage(auto *ws, std::string_view message, uWS::OpCode opCode){
+        if (message.length() >= sizeof(carta::EventHeader))
+        {
+            carta::EventHeader head = *reinterpret_cast<const carta::EventHeader *>(message.data());
+            const char *event_buf = message.data() + sizeof(carta::EventHeader);
+            int event_length = message.length() - sizeof(carta::EventHeader);
+            CARTA::EventType event_type = static_cast<CARTA::EventType>(head.type);
+            std::cout << event_type << "!\n";
+
+            switch (head.type)
+            {
+            case CARTA::EventType::REGISTER_VIEWER:
+            {
+                CARTA::RegisterViewer message;
+
+                if (message.ParseFromArray(event_buf, event_length))
+                {
+                    std::cout << "GOOD" << std::endl;
+                    // session->OnRegisterViewer(message, head.icd_version, head.request_id);
+                }
+                else
+                {
+                    std::cout<<"BAD" << std::endl;
+
+                }
+                break;
+            }
+            }
+        }
+        ws->send(message, opCode, true);
     }
 };
 
