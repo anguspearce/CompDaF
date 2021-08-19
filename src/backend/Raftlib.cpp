707e58e47ae185a6682bc2529a993061811db501
georgeo30 <georgeosabu@gmail.com>
2021-08-19 11:27:38 +0200

    Simple raftlib Sum implementation. Co-authored-by: Angus Pearce <anguspearce@users.noreply.github.com>

--- src/backend/Raftlib.cpp
+++ src/backend/Raftlib.cpp
@@ -1,31 +1,34 @@
 #include "Raftlib.h"
 #include "Sum.h"
-#include "Reader.h"
+//#include "Reader.h"
+#include <raft>
 
-double Raftlib::sum(std::vector<std::int64_t> &vec){
+double Raftlib::sum(std::vector<double> &vec)
+{
     /** define type for streams **/
-   using type_t = std::vector<std::int64_t>;
+    using type_t = std::vector<double>;
 
-   /** this will be our worker 'sum' kernel **/
-   using sum    = Sum< type_t >;
-   sum s;
+    /** this will be our worker 'sum' kernel **/
+    using sum = Sum<type_t>;
+    sum s;
 
-   using reader = Reader <type_t>;
-   reader r(vec);
-   
-   raft::map m;
-   /** Linking the vector to the input of the kernel**/
-   m += r >> s[ "input_arr" ];
-   /** take the only output port of s and link it to the only input port of p **/
-   auto total = 0;
-   m += s >> total;
-   /** 
+    using reader = Reader <type_t>;
+    reader r(vec);
+    //auto readeachone(raft::read_each<double>(vec.begin(), vec.end()));
+    using print = raft::print<type_t, '\n'>;
+    print p;
+    raft::map m;
+    /** Linking the vector to the input of the kernel**/
+    m += r >> s["input_arr"];
+    /** take the only output port of s and link it to the only input port of p **/
+    //auto total = 0;
+    m += s >> p;
+    /** 
     * NOTE: this will be going away soon, 
     * to be called on scope exit, an explicit
     * barrier call will enable integration with
     * sequential code.
     */
-   m.exe();
-   return( EXIT_SUCCESS );
+    m.exe();
+    return (EXIT_SUCCESS);
 }
-
--- src/backend/Raftlib.h
+++ src/backend/Raftlib.h
@@ -1,14 +1,15 @@
 #ifndef RAFTLIB_H
 #define RAFTLIB_H
 
+#include <vector>
+
 class Raftlib{
     public:
-        double sum(std::vector<std::int64_t> &vec);
+        double sum(std::vector<double> &vec);
         double mean();
         double stdDev();
         double max();
         double min();
-        std::vector<std::int64_t> splitAndSum();
 
 };
 
